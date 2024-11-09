ae458b439482369d099e7c9168a087af1070f87f
rayxtn <houimliraed@outlook.fr>
2024-11-09 02:58:07 +0100

    guess game

--- guess.py
+++ guess.py
@@ -1,1 +1,15 @@
-dfsdgsd
+import random #first thing we need to import random to generate the random number we need
+#generating a random number between 0 and 100
+game_number = random.randrange(0,100)
+ #user need to specify a number in an input
+user_input =  input('guess the number ! ')
+user_inputInt = int(user_input)
+while(user_inputInt != game_number): 
+    if(user_inputInt > game_number):        
+        print('too high')
+        break
+    elif(user_inputInt < game_number):
+        print('too low')    
+        break
+    else:
+        print('you guessed right!')
--- rolld.py
+++ rolld.py
@@ -5,7 +5,7 @@ while(True):
     if choice == 'y':
         dice_roll1 = random.randrange(1,6)
         dice_roll2 = random.randrange(1,6)
-        print('Value of dice is',{dice_roll1},{dice_roll2})
+        print('Value of dice is' ,{dice_roll1},{dice_roll2})
     elif choice == 'n':
         print('thanks for playing !')
         break
