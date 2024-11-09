420936b61370860010bb5a9512d56f4b3f4f593a
rayxtn <houimliraed@outlook.fr>
2024-11-09 03:30:29 +0100

    fixed guess

--- guess.py
+++ guess.py
@@ -1,15 +1,17 @@
 import random #first thing we need to import random to generate the random number we need
 #generating a random number between 0 and 100
 game_number = random.randrange(0,100)
- #user need to specify a number in an input
-user_input =  input('guess the number ! ')
-user_inputInt = int(user_input)
-while(user_inputInt != game_number): 
-    if(user_inputInt > game_number):        
+ #user_input need to specify a number in an input
+while(True):  
+    user_input =  input('guess the number ! ')
+    user_input = int(user_input)
+
+    if(user_input > game_number):        
         print('too high')
-        break
-    elif(user_inputInt < game_number):
+        
+    elif(user_input < game_number):
         print('too low')    
-        break
+        
     else:
         print('you guessed right!')
+        break
